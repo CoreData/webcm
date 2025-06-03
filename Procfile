ad340ce80f015e63e6afbe1dd3bb7da0c180a509
Christian Amey <1375363+CoreData@users.noreply.github.com>
2025-06-03 17:43:55 +0200

    Initial commit.

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: bundle exec ruby cm_sinatra.rb -p $PORT
---
+++ cm_sinatra.rb
@@ -0,0 +1,50 @@
+require "sinatra/base"
+require "./coffee_machine"
+
+class WebCoffeeMachine < Sinatra::Base
+
+  set :cm, CoffeeMachine.new
+
+  get '/' do
+   @info = settings.cm.info
+   erb :info
+  end
+
+  get '/clean' do
+    @info = settings.cm.clean
+    erb :info
+  end
+  
+  post '/refill_coffee' do
+    amount = params['coffee_input'].to_i
+    begin
+      @info = settings.cm.refill_coffee(amount)
+    rescue ToMuchCoffeeException
+     @info = "Zuviel Kaffee eingefuellt, bitte Fuellmenge reduzieren."
+    end
+    erb :info
+  end
+
+  post '/refill_water' do
+    amount = params['water_input'].to_i
+    begin
+     @info = settings.cm.refill_water(amount)
+    rescue ToMuchWaterException
+      @info ="Zuviel Wasser eingefuellt, bitte Fuellmenge reduzieren." 
+    end
+    erb :info
+  end
+
+  get '/make' do
+    begin
+      @info = settings.cm.make_coffee
+    rescue NoCoffeeException
+     @info = "Bitte Kaffee auffuellen."
+    rescue NoWaterException
+     @info = "Bitte Wasser auffuellen."
+    end
+    erb :info
+  end
+end
+
+WebCoffeeMachine.run!
